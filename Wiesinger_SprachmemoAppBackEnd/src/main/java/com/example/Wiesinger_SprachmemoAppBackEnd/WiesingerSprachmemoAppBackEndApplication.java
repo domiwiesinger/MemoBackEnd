880926c1fd9346109026187ecb184ad3a1cc174d
domiwiesinger <wiesingerdominik370@gmail.com>
2024-04-29 18:46:37 +0200

    Neues Repo erstellt und neues Spring Boot Projekt angelegt

---
+++ Wiesinger_SprachmemoAppBackEnd/src/main/java/com/example/Wiesinger_SprachmemoAppBackEnd/WiesingerSprachmemoAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.Wiesinger_SprachmemoAppBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiesingerSprachmemoAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiesingerSprachmemoAppBackEndApplication.class, args);
+	}
+
+}
